d285d02a1912e7d07fb1d05ae66e07af976b4ea2
scorpione71 <luigiorlando71@gmail.com>
2022-11-18 18:53:50 +0100

    varie agg

--- serverweb.go
+++ serverweb.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
 	//Gestore principale strumento HTTP
-	http.HandleFunc("/", luisHandler)
-	log.Fatal(http.ListenAndServe("localhost:9999", nil))
+	http.HandleFunc("/test/", luisHandler)
+	http.HandleFunc("/home", home_handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 	//Senza log di errori//
 	//http.ListenAndServe("localhost:9999", nil)//
 }
 
+func home_handler(w http.ResponseWriter, r *http.Request) {
+	page, _ := template.ParseFiles("Home.html")
+	page.Execute(w, "")
+}
+
 func luisHandler(response http.ResponseWriter, request *http.Request) {
 	//URL.Path[1:] serve a specificare che verrà visualizzato il path a partire dal secondo carattere, quindi visualizzerà tutto ciò che verra digitato dopo "/"
-	fmt.Fprintf(response, "ciao da %s\n", request.URL.Path[1:])
+	fmt.Fprintf(response, "HOME PAGE - LUIGI ORLANDO\n PATH DIGITATO [ %s ]", request.URL.Path[len("/test/"):])
 }
