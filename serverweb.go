f462a243b4e9c1d57af709dca5c3bfe03d63e1fe
scorpione71 <luigiorlando71@gmail.com>
2022-12-06 12:12:45 +0100

    oggi

--- serverweb.go
+++ serverweb.go
@@ -5,15 +5,43 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	//Gestore principale strumento HTTP
-	//Creazione Server Http Multiplexer
+	w := app.New()
+	a := w.NewWindow("ServerWeb")
+	dialogo := dialog.NewConfirm("ESCI", "Vuoi disattivare il Server ?", func(b bool) {
+		if b {
+			w.Quit()
+		}
+	}, a)
+	a.SetContent(container.NewGridWithRows(2, widget.NewButton("AVVIA SERVER", func() {
+		Multiplexer()
+	}), widget.NewButton("ESCI", func() {
+		dialogo.Show()
+	})))
+	a.Resize(fyne.NewSize(300, 200))
+	a.CenterOnScreen()
+	a.ShowAndRun()
+
+}
+
+func Multiplexer() {
+	//http Handler che è il Multiplexer
 	r := http.NewServeMux()
 	r.HandleFunc("/test/", luisHandler)
 	r.HandleFunc("/home", home_handler)
 	log.Fatal(http.ListenAndServe(":8080", r))
+
+	//Gestore principale strumento HTTP
+	//Creazione Server Http Multiplexer
+
 	/*Creazione server standard
 	http.HandleFunc("/test/", luisHandler)
 	http.HandleFunc("/home", home_handler)
